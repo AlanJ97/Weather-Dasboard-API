a5630e9517956c755c96a931bab13bca61371476
Alan <asegundo@spsolutions.com.mx>
2025-07-11 11:01:41 -0600

    Adding s3 backend

--- Weather-dashboard-API/infra/environments/dev/backend.tf
+++ Weather-dashboard-API/infra/environments/dev/backend.tf
@@ -1,2 +1,10 @@
-# This file is intentionally left blank for the dev environment.
+terraform {
+  backend "s3" {
+    bucket         = "weather-app-backend-terraform-bucket-2025"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
 
