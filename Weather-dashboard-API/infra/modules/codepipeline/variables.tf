498bd7ce2d8bea7622345ca65a330cc817c2d76a
Alan <asegundo@spsolutions.com.mx>
2025-07-16 11:36:23 -0600

    Changing branch to develop in CodePipeline

--- Weather-dashboard-API/infra/modules/codepipeline/variables.tf
+++ Weather-dashboard-API/infra/modules/codepipeline/variables.tf
@@ -18,7 +18,7 @@ variable "github_repo" {
 variable "github_branch" {
   description = "GitHub branch to trigger pipeline"
   type        = string
-  default     = "master"
+  default     = "develop"
 }
 
 variable "codebuild_project_name" {
